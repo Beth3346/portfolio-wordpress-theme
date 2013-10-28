51c66ff16b19b43953a3958cef2b18e709f421aa
Elizabeth Rogers <info@elizabeth-rogers.com>
2013-10-27 19:26:52 -0500

    changed div to section for widgets

--- includes/functions/_elr-register-sidebars.php
+++ includes/functions/_elr-register-sidebars.php
@@ -8,8 +8,8 @@ if ( function_exists('register_sidebar') ) {
 	register_sidebar(array(
 		'name' => 'Sidebar',
 		'id' => 'sidebar',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget' => '</section>',
 		'before_title' => '<h1 class="widgettitle">',
 		'after_title' => '</h1>',
 	));
@@ -17,8 +17,8 @@ if ( function_exists('register_sidebar') ) {
 	register_sidebar(array(
 		'name' => 'Footer 1',
 		'id' => 'footer',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget' => '</div>',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget' => '</section>',
 		'before_title' => '<h1 class="widgettitle">',
 		'after_title' => '</h1>',
 	));
