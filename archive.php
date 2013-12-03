5898d5b0eeeced67a95ea3ca173a06290bb397f7
Elizabeth Rogers <info@elizabeth-rogers.com>
2013-12-03 02:10:24 -0600

    added template files

---
+++ archive.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div class="main-content">
+	<article class="article" role="article">		
+		<?php // the loop ?>
+		<?php if (have_posts()) : ?>
+		
+			<?php while (have_posts()) : the_post(); ?>
+	
+				<?php get_template_part( 'includes/template-parts/_loop' , 'index'); ?>
+	
+			<?php endwhile; ?>
+							
+			<?php get_template_part( 'includes/template-parts/_pagination'); ?>
+		
+		<?php else : ?>
+	
+			<p><?php _e( 'Sorry, nothing found.', 'themify' ); ?></p>
+	
+		<?php endif; ?>			
+
+	</article>		
+	<?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
---
+++ page-no-sidebar.php
@@ -0,0 +1,23 @@
+<?php 
+	/*
+	Template Name: Single-Column
+	*/
+?>
+
+<?php get_header(); ?>
+<div class="main-content">
+	<article class="article-full" role="article">		
+	<?php while ( have_posts() ) : the_post(); ?>
+					
+		<h1 class="page-title"><?php the_title(); ?></h1>
+
+		<?php the_content(); ?>
+		
+		<?php wp_link_pages(array('before' => '<p><strong>'.__('Pages:','themify').'</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+		
+		<?php edit_post_link(__('Edit','themify'), '[', ']'); ?>
+
+	<?php endwhile; ?>	
+	</article>
+</div>
+<?php get_footer(); ?>
