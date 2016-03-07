cf9cf88069cf858b2ad64e8d1ac49dfe25d71b3d
Elizabeth Rogers <beth3346@gmail.com>
2016-03-06 20:27:24 -0600

    removed unused templates

--- archives/archive-news.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<main class="main-content elr-container">
-    <div class="elr-row">
-        <?php elr_normal_loop(); ?>
-    </div>
-</main>
-<?php get_footer(); ?>
--- archives/archive-service.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-<main class="main-content elr-container">
-    <?php elr_cpt_grid($wp_query, 'service', array('service_type'), true, true); ?>
-</main>
-<?php get_footer(); ?>
